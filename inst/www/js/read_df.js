5e1e7628ad3e406fad0fd5dd1b5e62847a45dc2e
punkba <prasadhsai07@gmail.com>
2019-02-26 11:10:06 +0530

    Integrate with R for first two slides

--- inst/www/js/read_df.js
+++ inst/www/js/read_df.js
@@ -37,15 +37,15 @@ $(document).ready(function(){
 
 		uploadcsv(filename);
 	  });
-	});
+});
 
-  function uploadcsv(filename){
+function uploadcsv(filename){
     $("#submitbutton").attr("disabled", "disabled");
     var req = ocpu.call("read_csv", {
 		file : filename,
     },function(session){
-		/*$("#status1").text("Got the file !!");
-		initiatePrelimAnalysis(session);*/
+		/*$("#status1").text("Got the file !!");*/
+		initiatePrelimAnalysis(session);
         alert('Read Successful');
     });
 
@@ -58,7 +58,7 @@ $(document).ready(function(){
     req.always(function(){
       $("#submitbutton").removeAttr("disabled")
     });
-  }
+}
 
 function initiatePrelimAnalysis(sessionData){
 
@@ -131,12 +131,15 @@ function createCheckBox(value,elem){
     // CREATE A TEXT NODE AND APPEND IT TO THE LABEL.
     lbl.appendChild(document.createTextNode(value));
 
-	discreteBox.appendChild(chk);
-	discreteBox.appendChild(lbl);
+	var divElement = document.createElement('div');
+	divElement.setAttribute('class','form-group');
+	divElement.appendChild(chk);
+	divElement.appendChild(lbl);
+
+	discreteBox.appendChild(divElement);
 
 	value = '';
 }
-
 
 var checkedVars = new Array();
 
@@ -156,114 +159,3 @@ function updateCheckList(variableName){
 		}
 	}
 }
-  function printsummary(mydata){
-	var req = ocpu.call("printsummary",
-						{"df_full" : mydata,
-						"dvname" : dvname,
-						"preddv" : preddv
-						},
-						function(session){
-							session.getObject(function(output){
-							$("#status1").text("Cleaning Successful! Setting up the results");
-
-		//FUNCTION CALLS TO SIGNIFICANT VARIABLE LIST & GRAPHS PLOTS
-	  plot_graph();
-      	  add_var_list();
-    }).fail(function(){
-      alert("Server error: " + req.responseText);
-    });
-  });
-  }
-  /*
-
-		//Adding code for var_list call
-
-		function add_var_list()
-			{
-				//alert("inside Add Options");
-				var vars;
-				var req = ocpu.call("imp_var_list", {	"target.var.name" : dvname},
-			function(session){
-				session.getObject(function(data){
-				//$("#output code").text(data);
-				//alert("imp_var_list ends: trying to append" );
-
-				for (var i=0; i < data.length;++i)
-				{
-					//alert("inside_for " + i);
-					var x = document.getElementById("DropList");
-					var option = document.createElement("option");
-					option.text = data[i];
-
-					x.add(option);
-
-					//alert("option " + i + " added")
-
-				}
-					alert("All set!")
-					$("#status1").text("Hurray! Go on and look at the results now...");
-				document.getElementById('DropList').onchange = function () {
-					var x = document.getElementById("DropList").selectedIndex;
-					var y = document.getElementById("DropList").options;
-
-						plot_graph_variable(y[x].text);
-
-				};
-				//plot_graph_variable()
-
-				}).fail(function(){
-					alert("R returned an error in var_list: " + req.responseText);
-					});
-				});
-
-				}
-
-
-		//VAR LIST CALL ENDS
-
-		//Adding Code for Significant variables graph
-				function plot_graph()
-			{
-				//alert("inside Plot graph");
-
-				//var req = $("#plotdiv1").rplot("randomplot", {	nfield : 100, distfield : "normal"})
-
-				var req = $("#plotdiv1").rplot("top_var_graph", {"target.var.name" : dvname, "ds" : ds });
-
-				//if R returns an error, alert the error message
-				req.fail(function(){
-				alert("Server error: " + req.responseText);
-				});
-
-				//after request complete, re-enable the button
-				req.always(function(){
-				$("#submitbutton").removeAttr("disabled")
-				});
-				//alert("plotted");
-			}
-
-
-			//SIGNIFICANT GRAPH CALL ENDS
-
-		function plot_graph_variable(tar)
-			{
-				//alert("inside variable Plot graph");
-
-				//var req = $("#plotdiv").rplot("randomplot", {	nfield : 100, distfield : "normal" , title : data})
-
-				var req = $("#plotdiv").rplot("variable_profiling_function", {dv : dvname, var: tar});
-
-				//if R returns an error, alert the error message
-				req.fail(function(){
-				alert("Server error: " + req.responseText);
-				});
-
-				//after request complete, re-enable the button
-				req.always(function(){
-				$("#submitbutton").removeAttr("disabled")
-				});
-				//alert("plotted");
-
-			}
-
-  */

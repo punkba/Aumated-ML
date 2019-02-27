3f4be62390139215d9b1f43e214fb825bff1eb7d
punkba <prasadhsai07@gmail.com>
2019-02-27 16:33:11 +0530

    Updated change

--- inst/www/js/model_df.js
+++ inst/www/js/model_df.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     var modelLink = "";
 	var modelSummaryPath="";
 
-	isChecked="";
+	isChecked=$('input[name=customRadio]:checked', '#modelsList').val();
 
 	$('#modelsList input').on('change', function() {
 		isChecked = $('input[name=customRadio]:checked', '#modelsList').val();

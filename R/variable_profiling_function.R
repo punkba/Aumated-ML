533efcf802ae36d9b7349eeef202c7614fa3ad8e
punkba <prasadhsai07@gmail.com>
2019-02-27 16:36:28 +0530

    Added event rate

--- R/variable_profiling_function.R
+++ R/variable_profiling_function.R
@@ -13,18 +13,18 @@ variable_profiling_function  <- function(dv, vars) {
   freq <- table(var1,dv)
   total <- freq[,"0"]+freq[,"1"]
   meaniv = freq[,"1"]/total
-   ggplot(dat, aes(var1, fill=dv)) + geom_bar(stat="count",position=position_dodge()) +
-   labs(title = "Bivariate Analysis") + 
-   xlab(vars) +
-   geom_hline(aes(yintercept = mean(meaniv)*100)) +
-   theme(
-     panel.background = element_rect(fill = "aliceblue",
-                                     colour = "lightblue",
-                                     size = 0.5),
-     panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                     colour = "white"), 
-     panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                     colour = "lightblue")
-   )
   
+  df1=as.data.frame(total)
+  df1$meaniv=meaniv
+  df1$levels=unique(dat[,vars])
+  
+  ggplot()+geom_col(data=df1, aes(y=total,x=levels))+labs(title = "Bivariate Analysis") + xlab(vars) +geom_line(data=df1, aes(y=meaniv*100,x=levels))+theme(
+    panel.background = element_rect(fill = "aliceblue",
+                                    colour = "lightblue",
+                                    size = 0.5),
+    panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                    colour = "white"), 
+    panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                    colour = "lightblue")
+  )+scale_y_continuous(sec.axis = sec_axis(~.*.75, name = "[%] Event Rate"))
 }

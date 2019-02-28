ebd60c82a329b13d54acbb42b59ce18b52b54601
punkba <prasadhsai07@gmail.com>
2019-02-28 12:03:08 +0530

    Changes for event rate addition

--- R/variable_profiling_function.R
+++ R/variable_profiling_function.R
@@ -16,21 +16,17 @@ variable_profiling_function  <- function(dv, vars) {
 
   df1=as.data.frame(total)
   df1$meaniv=meaniv
-  df1$levels=unique(dat[,vars])
+  df1$levels=rownames(df1)
+  df1$freq=freq[,"1"]
 
-  ggplot()+
-  geom_col(data=df1, aes(y=total,x=levels))+
-  labs(title = "Bivariate Analysis") +
-  xlab(vars) +
-  geom_line(data=df1, aes(y=meaniv*100,x=levels))+
-  geom_hline(aes(yintercept = mean(meaniv)*100))+
-  theme(
+  ggplot()+geom_col(data=df1, aes(y=total,x=levels))+labs(title = "Bivariate Analysis") + xlab(vars) +geom_hline(aes(yintercept = mean(meaniv)*max(total)))+geom_line(
+    data=df1, aes(y=meaniv*max(total),x=levels, group=1))+theme(
       panel.background = element_rect(fill = "aliceblue",
-                                    colour = "lightblue",
-                                    size = 0.5),
+                                      colour = "lightblue",
+                                      size = 0.5),
       panel.grid.major = element_line(size = 0.5, linetype = 'solid',
                                       colour = "white"),
       panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
                                       colour = "lightblue")
-    )+scale_y_continuous(sec.axis = sec_axis(~.*.75, name = "[%] Event Rate"))
+    )+scale_y_continuous(sec.axis = sec_axis(~./max(total)*100, name = "[%] Event Rate"))
 }

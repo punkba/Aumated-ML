347374402394c69ddcac5345e4c7f75c018936bf
punkba <prasadhsai07@gmail.com>
2019-02-28 16:19:51 +0530

    Update imp_var_list.R

--- R/imp_var_list.R
+++ R/imp_var_list.R
@@ -8,8 +8,8 @@ imp_var_list<- function(target.var.name){
   
   names(data)[names(data)==target.var.name] <- "DV"
   options(java.parameters = "-Xmx1g")
-  options(java.home="C:\\Program Files\\Java\\jre1.8.0_181")
-  Sys.setenv(JAVA_HOME="C:\\Program Files\\Java\\jre1.8.0_181")
+  options(java.home="C:\\Program Files\\Java\\jre1.8.0_201")
+  Sys.setenv(JAVA_HOME="C:\\Program Files\\Java\\jre1.8.0_201")
   data$DV<- as.integer(data$DV)
   options(warn=-1)  
 

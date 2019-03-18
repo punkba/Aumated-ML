1deeb1d29c1a901e0172c1ab08bf2f2c7ff2259a
punkba <prasadhsai07@gmail.com>
2019-03-18 15:04:48 +0530

    New

---
+++ R/identifyDVColumns.R
@@ -0,0 +1,23 @@
+identifyDVColumns <- function(inputFile){
+  
+  library(utils)
+  if(!grepl(".csv$", inputFile)){
+    stop("Uploaded filename must be a .csv filename!");
+  }
+  
+  file<-utils::read.csv(inputFile, header = TRUE, stringsAsFactors = FALSE )
+  
+  #get the column names
+  uniqueValuesByColumn <- lapply(file,function(x) length(unique(x)))
+  
+  #filter column names only with 2 levels
+  uniqueValuesByColumn <- uniqueValuesByColumn[uniqueValuesByColumn == 2]
+  
+  if(uniqueValuesByColumn == 0){
+    stop('File does not contain a dependent variable columns with 2 categories')
+  }
+  else{
+    return(names(uniqueValuesByColumn))
+  }  
+}
+
